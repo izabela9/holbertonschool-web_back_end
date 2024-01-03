7df97af0eab8bb62a154a7da6bda912e12cf1a4b
izabela9 <isabelanelaj1@gmail.com>
2024-01-03 16:47:59 +0100

    float to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float
+    """
+    return str(n)
