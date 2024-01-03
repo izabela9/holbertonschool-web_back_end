f512bb7cea2af50deee9a8db0aa6fad4f8f738e4
izabela9 <isabelanelaj1@gmail.com>
2024-01-03 15:18:39 +0100

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float as an integer
+    """
+    return int(n)
