d368edb463f02008f43d26939c46d4fb44f63e59
izabela9 <isabelanelaj1@gmail.com>
2024-01-03 15:10:37 +0100

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings 
+    """
+
+    return str1 + str2
