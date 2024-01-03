d4ea9e529414d519ad7e83ea9c94dc0fe1926fa7
izabela9 <isabelanelaj1@gmail.com>
2024-01-03 14:55:17 +0100

    add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-"""
-Simple model with  basic annotations - add
-"""
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
 
 
 def add(a: float, b: float) -> float:
