3f0ffd9eb55e6a99cc3ac38556c8b1f9f68121a7
izabela9 <isabelanelaj1@gmail.com>
2024-01-04 14:04:51 +0100

    tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Function that returns the list of
     all the delays in ascending order
