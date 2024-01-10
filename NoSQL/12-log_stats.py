91f25fbe1bf38b30cb4c9f73e740a495ebc5ceaa
izabela9 <isabelanelaj1@gmail.com>
2024-01-10 13:32:20 +0100

    log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module to print status of logs and 
+most Methods
+'''
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient()
+    logs_collection = client.logs["nginx"]
+
+    print(f"{logs_collection.count_documents({})}")
+    print("Methods:")
+    print(f"\tmethod GET: {logs_collection.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {logs_collection.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {logs_collection.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {logs_collection.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {logs_collection.count_documents({'method': 'DELETE'})}")
+    print(f"{logs_collection.count_documents({'path': '/status'})} status check")
