de7663bc863cbbdf88b790866b0dddb38b66cc58
izabela9 <isabelanelaj1@gmail.com>
2024-01-18 06:16:05 -0800

    adding properties

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,9 @@
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
-    ['income-${getCurrentYear()}']: income,
-    ['gdp-${getCurrentYear()}']: gdp,
-    ['capita-${getCurrentYear()}']: capita,
+  const currentYear = new Date().getFullYear();
+  return {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+    year: currentYear,
   };
-  return budget;
 }
