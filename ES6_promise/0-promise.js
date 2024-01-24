4a06956ba79c8df23496ebf0832bb75de7ec362b
izabela9 <isabelanelaj1@gmail.com>
2024-01-24 03:30:56 -0800

    first promise

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,15 @@
-function getResponseFromAPI() {
-  return new Promise((resolve) => {
+function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve();
+      if (success) {
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
     }, 10);
+	  .catch((error) => {
+      console.error('Error in getResponseFromAPI:', error.message);
   });
+});
 }
-
 export default getResponseFromAPI;
