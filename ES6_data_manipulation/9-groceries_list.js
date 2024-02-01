10d3f0b7b3fdd73f809eb97342ca24698fbae761
izabela9 <isabelanelaj1@gmail.com>
2024-02-01 11:08:38 +0000

    map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+
+export default function groceriesList() {
+	return new Map([
+		['Apples', 10],
+		['tomatoes', 10],
+		['Pasta', 1],
+		['Rice', 1],
+		['Banana', 5],
+	]);
+}
