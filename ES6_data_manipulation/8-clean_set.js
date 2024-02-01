c5a836264514360743b4ce1053441d93d66e214e
izabela9 <isabelanelaj1@gmail.com>
2024-02-01 10:59:12 +0000

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 /*eslint-disable*/
 
 export default function cleanSet(set, startString) {
+	if (typeof startString !== 'string') {
+    	return '';
+	}
 	const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
 	const resultString = filteredValues.map(value => value.slice(startString.length)).join('-');
 
